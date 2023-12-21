53b215c39a2a853ed9a1ba9d28aa2f840bed7866
JR <105200156+Zetsubou3041@users.noreply.github.com>
2023-12-21 16:23:52 +0800

    Add files via upload

---
+++ feb 17 - Copy/main.js
@@ -0,0 +1,13 @@
+
+item_1_img = document.getElementById('hoverimage').getAttribute('src');
+
+
+
+function item_1_hover() {
+    document.getElementById('hoverimage').src = 'images/1959 Les Paul Standard.png';
+}
+function item_1_leave() {
+    document.getElementById('hoverimage').src = ;
+}
+
+console.log(item_1_img);
